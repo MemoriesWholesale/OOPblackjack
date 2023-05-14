6a2feee20d457800fce6c3af6c9f5cd40d299d44
Chandramani Ramachandran <chandramani.ramachandran@gmail.com>
2023-05-14 00:47:40 -0700

    added ascii characters for face cardimages

--- blackjack.py
+++ blackjack.py
@@ -33,23 +33,56 @@ def totals(cards):
                 acesums = [s + 1 for s in acesums] + [s + 11 for s in acesums]
             return set(acesums)
 
+ornaments = font.Font(family = 'Bodoni Ornaments',size=20)
+
 class Card:
     def __init__(self,name):
         self.rank = name[:-1]
         self.suit = name[-1]
     def display(self,position):
-        cardstock = Canvas(table,bg='white',height=200,width=125)
-        cardstock.create_text(20,5,anchor=NE,text=self.rank,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font='Batang 12 bold')
-        cardstock.create_text(20,20,anchor=NE,text=self.suit,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 12 bold')
-        if self.rank.isdigit():
-            cardstock.create_text(40,100,text='\n\n'.join([self.suit for i in range(int(self.rank)//2)]),fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 16 bold')
-            cardstock.create_text(90,100,text='\n\n'.join([self.suit for i in range(int(self.rank)//2)]),fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 16 bold')
-            if int(self.rank)%2 != 0:  
-                cardstock.create_text(65,100,text=self.suit,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 16 bold')
-        elif self.rank == 'A':
-            cardstock.create_text(65,100,text=self.suit,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 60 bold')
+        if self.rank in {'J','Q','K'}:
+            cardstock = Canvas(table,bg='white',height=200,width=125)
+            cardstock.create_text(65,185,text='AYZAYZA',fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font=ornaments)
+            if self.rank == 'K':
+                if self.suit == '♠':
+                    cardstock.create_text(65,50,text='🂮',fill="black",font= 'Batang 210 bold')
+                elif self.suit == '♥':   
+                    cardstock.create_text(65,50,text='🂾',fill="red",font= 'Batang 210 bold')
+                elif self.suit == '♦':
+                    cardstock.create_text(65,50,text='🃎',fill="red",font= 'Batang 210 bold')      
+                elif self.suit == '♣':
+                    cardstock.create_text(65,50,text='🃞',fill="black",font= 'Batang 210 bold')    
+            elif self.rank == 'Q':
+                if self.suit == '♠':
+                    cardstock.create_text(65,50,text='🂭',fill="black",font= 'Batang 210 bold')
+                elif self.suit == '♥':   
+                    cardstock.create_text(65,50,text='🂽',fill="red",font= 'Batang 210 bold')           
+                elif self.suit == '♦':
+                    cardstock.create_text(65,50,text='🃍',fill="red",font= 'Batang 210 bold')  
+                elif self.suit == '♣':
+                    cardstock.create_text(65,50,text='🃝',fill="black",font= 'Batang 210 bold')
+            else:
+                if self.suit == '♠':
+                    cardstock.create_text(65,50,text='🂫',fill="black",font= 'Batang 210 bold')
+                elif self.suit == '♥':   
+                    cardstock.create_text(65,50,text='🂻',fill="red",font= 'Batang 210 bold')             
+                elif self.suit == '♦':
+                    cardstock.create_text(65,50,text='🃋',fill="red",font= 'Batang 210 bold')                 
+                elif self.suit == '♣':
+                    cardstock.create_text(65,50,text='🃛',fill="black",font= 'Batang 210 bold')
+
         else:
-            cardstock.create_text(65,100,text=self.rank,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 60 bold')
+            cardstock = Canvas(table,bg='white',height=200,width=125)
+            cardstock.create_text(20,5,anchor=NE,text=self.rank,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font='Batang 12 bold')
+            cardstock.create_text(20,20,anchor=NE,text=self.suit,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 12 bold')
+            if self.rank.isdigit():
+                cardstock.create_text(40,100,text='\n\n'.join([self.suit for i in range(int(self.rank)//2)]),fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 16 bold')
+                cardstock.create_text(90,100,text='\n\n'.join([self.suit for i in range(int(self.rank)//2)]),fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 16 bold')
+                if int(self.rank)%2 != 0:  
+                    cardstock.create_text(65,100,text=self.suit,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 16 bold')
+            else:
+                cardstock.create_text(65,100,text=self.suit,fill=f"{'red' if self.suit in {'♥', '♦'} else 'black'}",font= 'Batang 60 bold')
+    
         cardstock.place(in_= table, relx = position[0], rely = position[1], anchor = CENTER)
     def displayback(self,position):
         cardstock = Canvas(table,bg='red',height=200,width=125)
